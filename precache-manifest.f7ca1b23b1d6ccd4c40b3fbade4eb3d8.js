0be8ca3acb5b493b8ae9f8bd0afe28a3924a0ae2
AKYS TRINH <anhkysuvndn@gmail.com>
2019-10-15 17:01:07 +0700

    Updates

---
+++ precache-manifest.f7ca1b23b1d6ccd4c40b3fbade4eb3d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7bcc82f091c513d0c0751cfc74bc38",
+    "url": "/ghpages-testing/index.html"
+  },
+  {
+    "revision": "7d2d39cb16d5da36261f",
+    "url": "/ghpages-testing/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0e8f0ff519976b87aa0d",
+    "url": "/ghpages-testing/static/js/2.2673c42b.chunk.js"
+  },
+  {
+    "revision": "7d2d39cb16d5da36261f",
+    "url": "/ghpages-testing/static/js/main.e05519b8.chunk.js"
+  },
+  {
+    "revision": "d638f57c772489e3e0e7",
+    "url": "/ghpages-testing/static/js/runtime-main.d7afffa3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ghpages-testing/static/media/logo.25bf045c.svg"
+  }
+]);
